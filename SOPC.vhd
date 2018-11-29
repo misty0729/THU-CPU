6c28ce7a338af3925afcea6a4fc6096f763a3eb1
ZYFZYF <973446154@qq.com>
2018-11-29 05:41:00 -0800

    QAQAQ

--- SOPC.vhd
+++ SOPC.vhd
@@ -73,6 +73,7 @@ signal load_finish: STD_LOGIC;
 signal rst_for_cpu:	STD_LOGIC;
 signal rom_fail: STD_LOGIC;
 signal ram_fail: STD_LOGIC;
+signal rom_sucess: STD_LOGIC;
 
 --ROM_NEED
 signal rom_ce :  STD_LOGIC;
@@ -150,7 +151,7 @@ Port(   rst:                in  STD_LOGIC;
 		  tbre:               in  STD_LOGIC;
         tsre:               in  STD_LOGIC;
         data_ready:         in  STD_LOGIC;
-		  ram_to_rom:			 out STD_LOGIC;
+		  rom_success:			 out STD_LOGIC;
 
         load_finish:        out STD_LOGIC;
 		  dyp:					out STD_LOGIC_VECTOR(6 downto 0));
@@ -178,19 +179,20 @@ begin
                     end if;
                 end process;
 	 ram_fail <= '0';
+	 rom_fail <= not(rom_sucess);
 	 rst_for_cpu <= rst and load_finish;
-    CPU_component: CPU port map(clk=>clk, rst=>rst_for_cpu, rom_read_data_in=>rom_read_data_in, rom_ce=>rom_ce, rom_addr=>rom_addr,
+    CPU_component: CPU port map(clk=>clk_step, rst=>rst_for_cpu, rom_read_data_in=>rom_read_data_in, rom_ce=>rom_ce, rom_addr=>rom_addr,
                                 ram_read_data_in=>ram_read_data_in, ram_ce=>ram_ce, ram_we=>ram_we, ram_write_data_out=>ram_write_data_out, ram_addr=>ram_addr,
                                 led=>led, dyp0=>fakedyp, dyp1=>dyp1, stallreq_from_if=>rom_fail, stallreq_from_mem=>ram_fail);
 
 --    ROM_component: ROM port map(addr=>rom_addr, ce=>rom_ce, data=>rom_read_data_in);
 
 --    RAM_component: RAM port map(clk=>clk, ce=>ram_ce, we=>ram_we, data_in=>ram_write_data_out, addr=>ram_addr, data_out=>ram_read_data_in);
-	 RomRam_component: RomRam port map(clk=>clk, rst=>rst, 
+	 RomRam_component: RomRam port map(clk=>clk_4, rst=>rst, 
 													rom_ce=>rom_ce, rom_addr=>rom_addr, rom_read_data=>rom_read_data_in,
 													ram_ce=>ram_ce, ram_we=>ram_we, ram_addr=>ram_addr, ram_write_data=>ram_write_data_out, ram_read_data=>ram_read_data_in,
 													Ram1EN=>Ram1EN, Ram1OE=>Ram1OE, Ram1WE=>Ram1WE, Ram1Addr=>Ram1Addr, Ram1Data=>Ram1Data, wrn=>wrn, rdn=>rdn,
 													Ram2EN=>Ram2EN, Ram2OE=>Ram2OE, Ram2WE=>Ram2WE, Ram2Addr=>Ram2Addr, Ram2Data=>Ram2Data,
-													load_finish=>load_finish, tbre=>tbre, tsre=>tsre, data_ready=>data_ready, dyp=>dyp0, ram_to_rom=>rom_fail);
+													load_finish=>load_finish, tbre=>tbre, tsre=>tsre, data_ready=>data_ready, dyp=>dyp0, rom_success=>rom_sucess);
 end Behavioral;
 
