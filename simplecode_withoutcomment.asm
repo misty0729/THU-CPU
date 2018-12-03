6e0f5684268a57b0171f83d011eba3b559b6c824
qianzgit <qianz_need@163.com>
2018-12-03 18:35:05 +0800

    增加了编辑器的mips代码

---
+++ simplecode_withoutcomment.asm
@@ -0,0 +1,93 @@
+INIT: LI R0 0                   
+LI R1 0                         
+LI R2 0                         
+LI R3 0                         
+CLEARSCREEN: LI R4 9            
+SLL R4 R4 0                     
+ADDIU R4 60                     
+CLEARCHAR: SUBU R4 R2 R5        
+BEQZ R5 SETCURSORLEFTTOP        
+NOP
+LI R5 BF
+SLL R5 R5 0                     
+SW R5 R1 5
+SW R5 R2 4
+ADDIU R2 1                      
+B CLEARCHAR                     
+NOP
+SETCURSORLEFTTOP: LI R2 0       
+LI R5 BF
+SLL R5 R5 0
+SW R5 R2 7
+GETCURRENTKEY: MOVE R0 R1       
+LI R5 BF                        
+SLL R5 R5 0
+LW R5 R1 6
+SUBU R0 R1 R5                   
+BEQZ R5 GETCURRENTKEY           
+NOP
+BEQZ R1 GETCURRENTKEY           
+NOP
+LI R5 8                        
+SLTU R1 R5
+BTEQZ E                         
+NOP
+LI R4 4F                        
+CMP R3 R4
+BTEQZG ETCURRENTKEY             
+NOP
+ADDIU R3 1                      
+LI R5 BF                        
+SLL R5 R5 0
+SW R5 R1 5
+SW R5 R2 4
+ADDIU R2 1                      
+SW R5 R2 7
+B GETCURRENTKEY                 
+NOP
+JUDGEBKSP: LI R5 8            
+SUBU R5 R1 R5
+BNEZ R5 JUDGECLEARSCREEN        
+NOP
+LI R4 0                         
+CMP R3 R4
+BTEQZ GETCURRENTKEY             
+NOP
+ADDIU R3 FF                     
+LI R5 BF                        
+SLL R5 R5 0
+ADDIU R2 FF                     
+SW R5 R2 7
+SW R5 R1 5 
+SW R5 R2 4
+B GETCURRENTKEY                 
+NOP
+//JUDGECLEARSCREEN: LI R5 d      
+//SUBU R5 R1 R5
+//BNEZ R5 JUDGEEXIT               
+//NOP
+//B INIT                          
+//NOP
+JUDGEEXIT: LI R5 10             
+SUBU R5 R1 R5
+BNEZ R5 JUDGEENTER              
+NOP
+B QUIT                          
+NOP
+JUDGEENTER: LI R5 d            
+SUBU R5 R1 R5
+BNEZ R5 NEXT                    
+NOP
+LI R4 50                        
+SUBU R4 R3 R4
+ADDU R2 R4 R2                   
+LI R3 0                         
+LI R5 BF                        
+SLL R5 R5 0
+SW R5 R2 7
+B GETCURRENTKEY                 
+NOP
+NEXT: B GETCURRENTKEY           
+NOP
+QUIT: JR R7
+NOP
